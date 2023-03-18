8b1510bb585751d9fabf402ed388f252504623d1
Derick Ndlovu <ndlovudcp@gmail.com>
2023-03-18 09:08:53 -0700

    Displaying HEX

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - the start of the program
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char n, c;
+
+	n = '1';
+	c = 'a';
+	while
+		(n <= '9') {
+			putchar(n);
+			n++;
+		}
+	while 
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
