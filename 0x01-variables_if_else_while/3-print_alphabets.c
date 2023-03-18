6cff6b130c04ee53665c7f20e6110b02517d325c
Derick Ndlovu <ndlovudcp@gmail.com>
2023-03-18 06:32:36 -0700

    Lowercase and UPPERCASE

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - the beginning of the program
+ *
+ * Return: njalo u 0
+ *
+ */
+
+int main(void)
+{
+	char alph, ABET;
+
+	alph = 'a';
+	ABET = 'A';
+	while
+		(alph <= 'z') {
+			putchar(alph);
+			alph++;
+		}
+	while 
+		(ABET <= 'Z') {
+			putchar(ABET);
+			ABET++;
+		}
+	putchar('\n');
+
+	return (0);
+}
