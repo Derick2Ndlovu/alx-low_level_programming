faaec91160d0713d6406290e119c66607aefd04d
Derick Ndlovu <ndlovudcp@gmail.com>
2023-03-18 08:42:59 -0700

    Displaying number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - the main of the program
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while
+		(n < 10) {
+			putchar(n + 0);
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
