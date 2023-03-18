0744eedfc78ba1e2da406fb8171fb54aa3d30b25
Derick Ndlovu <ndlovudcp@gmail.com>
2023-03-18 05:28:50 -0700

    Printing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - main entryof the program
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+	char alpha;
+	alpha = 'a';
+	while 
+		(alpha <= 'z')
+			{
+				putchar(alpha);
+				alpha++;
+			
+
+			}
+	return(0);
+}
