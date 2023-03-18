2e32f2d6ae3a30f683bb13b371a803b7e2359222
Derick Ndlovu <ndlovudcp@gmail.com>
2023-03-18 08:17:26 -0700

    Displaying numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - the start of the program
+ *
+ * Return - njalo u 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while 
+		(n < 10) {
+			printf("%d", n);
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
