32aea761757932782f169afb9a39a0602eae7d98
Derick Ndlovu <ndlovudcp@gmail.com>
2023-03-18 06:45:43 -0700

    Printing alphabets except some

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - ukuqala kwe program
+ *
+ * Buyisa: buyisa njalo u 0
+ */
+
+int main(void)
+{
+
+	char alpha;
+
+	alpha = 'a';
+	while
+		(alpha <= 'z') {
+			putchar(alpha);
+			alpha++;
+		}
+	putchar('\n');
+	return (0);
+}
