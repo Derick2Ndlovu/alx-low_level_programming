a334c08150b662afebb9ad7a88bf9b7613905849
Derick Ndlovu <ndlovudcp@gmail.com>
2023-03-18 08:53:21 -0700

    Reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main of the program
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char alpha;
+
+	alpha = 'z';
+	while
+		(alpha >= 'a') {
+			putchar(alpha);
+			alpha--;
+		}
+	putchar('\n');
+	return (0);
+}
