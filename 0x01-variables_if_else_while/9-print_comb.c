b2cf97e036e462bffd1d0b84fa7578563f9f4816
Derick Ndlovu <ndlovudcp@gmail.com>
2023-03-19 06:53:49 -0700

    printing other numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main of the program
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while 
+		(n < 10) {
+			putchar(n + '0');
+			putchar(',');
+			putchar(' ');
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
