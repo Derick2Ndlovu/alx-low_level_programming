7a879006cc0b45446f0279065da1d71f9e3d2b32
Derick Ndlovu <ndlovudcp@gmail.com>
2023-03-19 03:54:29 -0700

    updating 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - the entry of the program
+ *
+ * Return: always 0
+ */
 
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
