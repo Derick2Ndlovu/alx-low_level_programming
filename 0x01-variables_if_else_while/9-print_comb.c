9b7b3aafd1c76274637ad9e14da23829ef6a3012
Derick Ndlovu <ndlovudcp@gmail.com>
2023-03-20 03:04:48 -0700

    updated print comb numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,13 @@ int main(void)
 {
 	int n;
 
-	n = 0;
-	while
-		(n <= 9) {
-			putchar(n + '0');
-			if (n <= 8)
-				putchar(',');
+	for (n = 0; n < 10; n++) {
+		putchar(n + '0');
+		if (n < 9) {
+			putchar(',');
 			putchar(' ');
-			n++;
 		}
+	}
 	putchar('\n');
 	return (0);
 }
