80058def056523b4053c24f496cf55a1efd487f2
Derick Ndlovu <ndlovudcp@gmail.com>
2023-03-20 14:16:30 -0700

    Computing the absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computing the absolute values of integer
+ * @i: integer value to computed
+ * Return: absolute value
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (-i);
+}
